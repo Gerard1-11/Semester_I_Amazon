ba82693f686a3e562f639cdd98a75204d1aa46a4
Gerardo ValQ <A01374994@itesm.mx>
2020-03-31 23:30:33 -0600

    ZergGame 1.2.0 (Ultima versión)

--- Graficas_Computacionales/ZergGameV2/ZergGame.js
+++ Graficas_Computacionales/ZergGameV2/ZergGame.js
@@ -1,14 +1,13 @@
 let container;
 let camera, scene, raycaster, renderer;
 
-let objectList = [];
-let objectList2 = [];
-let objCollBoxes = [];
-let objectCenterBBox, objectBBox;
-
 let root = null,
 group = null,
+objectList = [],
+objectList2 =[],
+objectList3 =[];
 orbitControls = null;
+let objectCenter;
 
 let objLoader = null;
 let duration = 5000; // ms
@@ -17,86 +16,198 @@ let currentTime = Date.now();
 let mouse = new THREE.Vector2(), INTERSECTED, CLICKED;
 let radius = 100, theta = 0;
 
-let puntaje = $('#puntaje');
-var puntos = "0"
+let puntosTot = 0;
 
-let vidas = 5;
+let floorUrl = "suelo1.jpg";
+let objModelUrl2 = {obj:'pokeballMod.obj', map:'PKtex.png'};
+let objModelUrl = {obj:'Mew_Pokemon_OBjMod.obj', map:'mew_eyes_Texture.png'};
 
-let floorUrl = "bkGround.jpg";
+function promisifyLoader ( loader, onProgress ) 
+{
+    function promiseLoader ( url ) {
+  
+      return new Promise( ( resolve, reject ) => {
+  
+        loader.load( url, resolve, onProgress, reject );
+  
+      } );
+    }
+  
+    return {
+      originalLoader: loader,
+      load: promiseLoader,
+    };
+}
+const onError = ( ( err ) => { console.error( err ); } );
 
-function animate() {		
+function moveX(object, speed) {
+    if(object.position.x > 0){
+        var d = object.position.x - objectCenter.position.x;
+        if (object.position.x > objectCenter.position.x) {
+            object.position.x -= Math.min( speed, d );
+        }
+    }else if (object.position.x < 0){
+        var d = object.position.x + objectCenter.position.x;
+        if (object.position.x < objectCenter.position.x) {
+            object.position.x += Math.max( speed, d );
+        }
+    }
+}
+
+function moveY(object, speed) {
+    console.log(object.position)
+    if(object.position.y > 0){
+        var d = object.position.y - objectCenter.position.y;
+        if (object.position.y > objectCenter.position.y) {
+            object.position.y -= Math.min( speed, d );
+        }
+    }else if (object.position.y < 0){
+        var d = object.position.y + objectCenter.position.y;
+        if (object.position.y < objectCenter.position.y) {
+            object.position.y += Math.max( speed, d );
+        }
+    }
+} 
+
+function moveZ(object, speed) {
+    if(object.position.z > 0){
+        var d = object.position.z - objectCenter.position.z;
+        if (object.position.z > objectCenter.position.z) {
+            object.position.z -= Math.min( speed, d );
+        }
+    }else if (object.position.z < 0){
+        var d = object.position.z + objectCenter.position.z;
+        if (object.position.z < objectCenter.position.z) {
+            object.position.z += Math.max( speed, d );
+        }
+    }
+}
+
+function setObjectPosition(object){
+    det =  Math.random()
+    det2 = Math.random()
+    det3 = Math.random()
+
+        
+    
+    if(det<.5){
+        if(det2<.5){
+            object.position.x = (Math.random() * -10)
+            object.position.z = 75 + (Math.random() * 75)
+            object.position.y = (Math.random() * -10)
+            
+        }else{
+            object.position.x = (Math.random() * -10)
+            object.position.z = 75 + (Math.random() * 50)
+            object.position.y = (Math.random() * 10)
+        }
+    }else{
+        if(det3<.5){
+            object.position.x = (Math.random() * 10)
+            object.position.z = 75 + (Math.random() * 25)
+            object.position.y = (Math.random() * -10)
+        }else{
+            object.position.x = (Math.random() * 10)
+            object.position.z = 75 + (Math.random() * 100)
+            object.position.y = (Math.random() * 10)
+        }
+    }
+    
+    object.rotation.y = -200;
+}
+
+function animate() {
+    
+    $("#puntaje").html("SCORE: " + puntosTot );
+
     let now = Date.now();
     let deltat = now - currentTime;
     currentTime = now;
     let fract = deltat / duration;
     let angle = Math.PI * 2 * fract;
 
-    objectCenterBBox.update();
-    knotBox = new THREE.Box3().setFromObject(objectCenterBBox);
-    
-     
-
-    
-    let index = 0;
     for(object of objectList){
         if(object){
-            
-            objCollBoxes[index] = new THREE.BoxHelper(object, 0x000000);
-            icoBox = new THREE.Box3().setFromObject(objCollBoxes[index]);
 
-            if(knotBox.intersectsBox(icoBox)){
-                scene.remove(object);
-                scene.remove(objCollBoxes[index])
-                console.log(vidas)
-                console.log(objectList.length)
-                console.log(objCollBoxes.length)
-            }
-
-            object.rotation.y -= angle / 4;
-            object.rotation.x += angle / 2;
-            object.rotation.z -= angle / 6;
-
-            if(object.position.x !=0 || object.position.x !=0){
-                det4 = Math.random()
-                if(det4 < 1/3){
-                    if(puntos < 1000){
-                        object.position.x += Math.sign(object.position.x) * -.1
-                    }else if(puntos >=1000 && puntos < 3000){
-                        object.position.x += Math.sign(object.position.x) * -.2
-                    }else if(puntos >=3000 && puntos < 8000){
-                        object.position.x += Math.sign(object.position.x) * -.4
-                    }else{
-                        object.position.x += Math.sign(object.position.x) * -.6
-                    }
-                }else if(det4<2/3 && det4>=1/3){
-                    if(puntos < 1000){
-                        object.position.z += Math.sign(object.position.z) * -.1
-                    }else if(puntos >=1000 && puntos < 3000){
-                        object.position.z += Math.sign(object.position.z) * -.2
-                    }else if(puntos >=3000 && puntos < 8000){
-                        object.position.z += Math.sign(object.position.z) * -.4
-                    }else{
-                        object.position.z += Math.sign(object.position.z) * -.6
-                    }
-                    
-                }else{
-                    continue
+            if(object.position.z == 0){
+                if(puntosTot>0){
+                    puntosTot-=1;
                 }
-            }else{
-                continue
+                setObjectPosition(object);
             }
-        }
-        index++;
-    }
 
-    objectList2[0].rotation.y += angle / 2;
-    objectList2[0].rotation.x += angle / 2;
-    objectList2[0].rotation.z += angle / 2;
+            object.rotation.x += angle * 2;
+            object.rotation.y += angle / 6;
+            object.rotation.z += angle * 2/3;
+            moveZ(object,.15);
+        } 
+    }   
+}
 
+async function loadMultObjs(objModelUrl, objectList, index)
+{
+    const objPromiseLoader = promisifyLoader(new THREE.OBJLoader());
 
-
-    
+    try {
+        const object = await objPromiseLoader.load(objModelUrl.obj);
         
+        let texture = objModelUrl.hasOwnProperty('map') ? new THREE.TextureLoader().load(objModelUrl.map) : null;
+        
+        object.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+                child.material.map = texture;
+                // child.material.normalMap = normalMap;
+                // child.material.specularMap = specularMap;
+            }
+        });
+
+        object.scale.set(.2, .2, .2);
+        let name = "objObject" + String(index);
+        object.name = name
+        setObjectPosition(object);
+        objectList.push(object);
+        scene.add(object);
+
+    }
+    catch (err) {
+        return onError(err);
+    }
+}
+
+async function loadObj(objModelUrl, objectList)
+{
+    const objPromiseLoader = promisifyLoader(new THREE.OBJLoader());
+
+    try {
+        const object = await objPromiseLoader.load(objModelUrl.obj);
+        
+        let texture = objModelUrl.hasOwnProperty('map') ? new THREE.TextureLoader().load(objModelUrl.map) : null;
+        
+        object.traverse(function (child) {
+            if (child instanceof THREE.Mesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+                child.material.map = texture;
+                // child.material.normalMap = normalMap;
+                // child.material.specularMap = specularMap;
+            }
+        });
+
+        object.scale.set(1, 1, 1);
+        object.position.z = 0;
+        object.position.y = 0;
+        object.position.x = 0;
+        object.name = "pokeball";
+        objectCenter = object;
+        objectList.push(object);
+        //scene.add(object);
+
+    }
+    catch (err) {
+        return onError(err);
+    }
 }
 
 function createScene(canvas) 
@@ -111,12 +222,12 @@ function createScene(canvas)
     // Options are THREE.BasicShadowMap, THREE.PCFShadowMap, PCFSoftShadowMap
     renderer.shadowMap.type = THREE.BasicShadowMap;
 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.set(0,200, 0);
+    camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 1, 50 );
+    camera.position.set(0, 0, -5);
     camera.lookAt(0,0,0);
     
     scene = new THREE.Scene();
-    scene.background = new THREE.TextureLoader().load(floorUrl);
+    scene.background = new THREE.Color( 0xf0f0f0 );
     
     let light = new THREE.DirectionalLight( 0xffffff, .40 );
     light.position.set( 1, 1, 1 );
@@ -143,73 +254,16 @@ function createScene(canvas)
     let floorGeometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
     let floor = new THREE.Mesh(floorGeometry, new THREE.MeshPhongMaterial({color:0xffffff, map:map, side:THREE.DoubleSide}));
     floor.rotation.x = -Math.PI / 2;
+    floor.position.y = -50
     //scene.add( floor );
 
-    let geometry = new THREE.IcosahedronBufferGeometry(15, 0);
-    let geometryCenter = new THREE.TorusKnotBufferGeometry( 20, 5, 200, 32 );
-    let objectCenter = new THREE.Mesh( geometryCenter, new THREE.MeshNormalMaterial() );
-    objectCenterBBox = new THREE.BoxHelper(objectCenter, 0x000000);
-    objectCenterBBox.update();
-    objectCenterBBox.visible = false;
-    objectList2.push(objectCenter)
+    scene.background = map; 
 
-    scene.add( objectCenter );
-    scene.add(objectCenterBBox);
-    for ( let i = 0; i < 100; i ++ ) 
+    loadObj(objModelUrl,objectList2)
+    
+    for ( let i = 0; i < 30; i ++ ) 
     {
-        let object = new THREE.Mesh( geometry, new THREE.MeshToonMaterial( { color: Math.random() * 0xffffff } ) );
-        let objectBBox = new THREE.BoxHelper(object, 0x000000);
-        objectBBox.update();
-        objectBBox.visible = false;
-        objCollBoxes.push(objectBBox)
-        
-        object.name = 'Ico_' + i;
-        det =  Math.random()
-        det2 = Math.random()
-        det3 = Math.random()
-
-        
-        if(det <.5){
-            if(det2<.5){
-                if(det3<.5){
-                    object.position.z = 150 + (Math.random() * 150)
-                    object.position.x = -100 + (Math.random() * -150)
-                }else{
-                    object.position.z = 100 + (Math.random() * 150)
-                    object.position.x = -150+ (Math.random() * -150)
-                }
-            }else{
-                if(det3<.5){
-                    object.position.z = -150+ (Math.random() * -150)
-                    object.position.x = -10+ (Math.random() * -150)
-                }else{
-                    object.position.z = -100+ (Math.random() * -150)
-                    object.position.x = -150+ (Math.random() * -150)
-                }
-            }
-        }else{
-            if(det2<.5){
-                if(det3<.5){
-                    object.position.z = 150+ (Math.random() * 150)
-                    object.position.x = 100+ (Math.random() * 150)
-                }else{
-                    object.position.z = 100+ (Math.random() * 150)
-                    object.position.x = 150+ (Math.random() * 150)
-                }
-            }else{
-                if(det3<.5){
-                    object.position.z = -150+ (Math.random() * -150)
-                    object.position.x = 100+ (Math.random() * 150)
-                }else{
-                    object.position.z = -100+ (Math.random() * -150)
-                    object.position.x = 150+ (Math.random() * 150)
-                }
-            }
-        }
-        
-        objectList.push(object)
-        scene.add( object );
-        scene.add(objectBBox);
+        loadMultObjs(objModelUrl2,objectList, i)
     }
     
     raycaster = new THREE.Raycaster();
@@ -236,7 +290,7 @@ function onDocumentMouseMove( event )
     // find intersections
     raycaster.setFromCamera( mouse, camera );
 
-    let intersects = raycaster.intersectObjects( scene.children );
+    let intersects = raycaster.intersectObjects( objectList, true);
     
     if ( intersects.length > 0 ) 
     {
@@ -246,18 +300,18 @@ function onDocumentMouseMove( event )
         {
             if ( INTERSECTED)
             {
-                //INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+                INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
             }
 
             INTERSECTED = intersects[ closer ].object;
-            //INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-            //INTERSECTED.material.emissive.setHex( 0xff0000 );
+            INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+            INTERSECTED.material.emissive.setHex( 0xff0000 );
         }
     } 
     else 
     {
         if ( INTERSECTED ) 
-            //INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+            INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
 
         INTERSECTED = null;
     }
@@ -272,21 +326,20 @@ function onDocumentMouseDown(event)
     // find intersections
     raycaster.setFromCamera( mouse, camera );
 
-    let intersects = raycaster.intersectObjects( scene.children );
+    let intersects = raycaster.intersectObjects( objectList, true);
 
     console.log("intersects", intersects);
     if ( intersects.length > 0 ) 
     {
         CLICKED = intersects[ intersects.length - 1 ].object;
-        //CLICKED.material.emissive.setHex( 0x00ff00 );
+        CLICKED.material.emissive.setHex( 0x00ff00 );
         console.log(CLICKED.name);
-        puntos = String(Number(puntos) + 100 )
-        console.log(puntos)
-        scene.remove(CLICKED)
-        index = ((CLICKED.name).split("_"))[1]
-        scene.remove(objCollBoxes[index])
-        console.log(objectList.length)
-        console.log(objCollBoxes.length)
+        //scene.remove(CLICKED.parent);
+        setObjectPosition(CLICKED.parent);
+        puntosTot += 1;
+        $("#puntaje").html("SCORE: " + puntosTot );
+        console.log(puntosTot)
+        
     } 
     else 
     {
@@ -299,14 +352,11 @@ function onDocumentMouseDown(event)
 //
 function run() 
 {
-    requestAnimationFrame( run );
+    requestAnimationFrame(() => run() );
     render();
-
-    animate();
-
-    puntaje.html("Puntaje: " + puntos);
-
     
+    
+    animate();
 }
 
 function render() 
